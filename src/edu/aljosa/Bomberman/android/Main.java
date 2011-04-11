397439af6d5f910fd6a7f3e03f23f993b855a21a
Apoloin <blazevic.aljosa@gmail.com>
2011-04-11 13:38:17 +0200

    Dela!

---
+++ src/edu/aljosa/Bomberman/android/Main.java
@@ -0,0 +1,26 @@
+package edu.aljosa.Bomberman.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    public void newGame()
+    {
+    
+    }
+    public void settings()
+    {
+    
+    }
+    public void exit()
+    {
+    	
+    }
+    
+}
